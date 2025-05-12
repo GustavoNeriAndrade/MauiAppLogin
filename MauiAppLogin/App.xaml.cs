9443c8c0bab5b39ed7aba7e158374e53237c62ee
Gustavo <gustavoneriandrade2007@gmail.com>
2025-05-11 22:42:46 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -5,8 +5,14 @@
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new Login();
         }
-    }
-}
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+        }
+    } // Fecha classe
+} // Fecha namespace
